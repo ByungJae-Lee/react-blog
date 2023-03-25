39e72dee5db9ecd5229349a151742504a35a6100
banya <bjgeumgang@gmail.com>
2023-03-25 20:51:53 +0900

    lec8

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
   
@@ -36,23 +37,27 @@ function App() {
       </div>
 
       <div className='list'>
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
-    <Modal></Modal>
+    {
+      modal === true ? <Modal/> : null
+    }
+    
+
     </div>
   );
 
     function Modal() {
       return (
-  <>
+  <div>
     <div className='modal'>
       <h4>제목</h4>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
-  </>
+  </div>
       )
     }
 }
