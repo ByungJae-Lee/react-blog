809eb6f5584a3b04f734c07a1fdf2741af6c80cc
banya <bjgeumgang@gmail.com>
2023-04-09 17:32:55 +0900

    과제-2

--- src/App.js
+++ src/App.js
@@ -15,28 +15,30 @@ function App() {
   
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학'])
   let [따봉, 따봉변경] = useState(0);
+  
 
   return (
+    
+
     <div className='App'>
 
       <div className= 'black-nav'>
         <h4>ReactBlog</h4> 
       </div>
 
-    <button onClick={()=> {
-      
-    }}>가나다순정렬</button>
+
+
+   <button onClick={()=> {
+    let copy = [...글제목];
+    copy.sort();
+    글제목변경(copy)
+   }}>가나다순정렬</button>
 
       <button onClick={()=> { 
         let copy = [...글제목];
         copy[0] = '여자 코트 추천'
         글제목변경(copy);
         }}>글수정</button>
-
-        <button onClick={()=>{
-
-        }}>
-          정렬</button>
       
       <div className = 'list'>
         <h4>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉+1) }}> 👍 </span> {따봉} </h4>
