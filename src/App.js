e42596d30b0bc22673b7e8794c289568aa4633ce
banya <bjgeumgang@gmail.com>
2023-04-26 20:42:39 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function 함수(){} 말고 다른 문법 ()=> {}을 사용하면 편리
   state는 등호로 변경금지 / state변경함수(새로운 state)
   */
 
+import { range } from 'range';
 import { useState } from 'react';
 import "./App.css";
 
@@ -15,8 +16,9 @@ function App() {
   
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학'])
   let [따봉, 따봉변경] = useState(0);
-  let [modal, setModal] = useState(true);
-  
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 //  
 //  반복문 축약하는 방법
 //    1. map함수를 사용
@@ -68,15 +70,39 @@ function App() {
       글제목.map(function(a, i){
         return (
         <div className = 'list' key={ i }>
-        <h4 onClick={ ()=> {setModal(true)}}> { 글제목[i] } 
-        <span onClick={()=>{따봉변경(따봉+1)}}> 👍 </span> {따봉} </h4>
+        <h4 onClick={ ()=> {setModal(true); setTitle(i)}}> 
+        { 글제목[i] } 
+        <span onClick={(e)=>{ e.stopPropagation(); 따봉변경(따봉+1)}}> 👍 </span> {따봉} </h4>
         <p>2월 17일 발행</p>
+        <button onClick={()=>{
+          let copy = [...글제목];
+          copy.splice(i, 1);
+          글제목변경(copy);
+        }}>삭제</button>
+
       </div>
       )})
     }
 
+    <button onClick={()=>{ setTitle(0) }}>글제목0</button>
+    <button onClick={()=>{ setTitle(1) }}>글제목1</button>
+    <button onClick={()=>{ setTitle(2) }}>글제목2</button>
+
+
+    <input onChange={(e)=>{
+      입력값변경(e.target.value);
+      console.log(입력값)
+    }}/>
+    <button onClick={()=>{ 
+      let copy = [...글제목];
+      copy.unshift(입력값);  
+      글제목변경(copy)
+    }}> 글발행 </button>
+
       {
-        modal === true ? <Modal 글제목변경={글제목변경} 글제목={글제목}/> : null
+        modal === true
+        ? <Modal title={title} 글제목={글제목}/>
+        : null
       }
 
 {/* 
@@ -102,11 +128,11 @@ function App() {
 
 
 function Modal(props) {
-  return(<div className='modal' style={{background : props.color}}>
-  <h4>{props.글제목[0]}</h4>
+  return(<div className='modal'>
+  <h4>{ props.글제목[props.title] }</h4>
   <p>날짜</p>
   <p>상세내용</p>
-  <button onClick={ ()=> { props.글제목변경(['여자 코트 추천','강남 우동 맛집','파이썬 독학']) } }>글수정</button>
+  <button >글수정</button>
 </div>)
 }
 
